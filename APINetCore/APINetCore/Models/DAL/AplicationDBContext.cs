27c271c8ca39ae48587bb0357bb1c79410a71ec4
Ever Enrique Lambraño Machado <everlm17@gmail.com>
2020-02-11 21:58:14 -0500

    Creacion del proyecto y modelo BD

---
+++ APINetCore/APINetCore/Models/DAL/AplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using APINetCore.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APINetCore.Models.DAL
+{
+    public class AplicationDBContext:DbContext
+    {
+        public AplicationDBContext(DbContextOptions<AplicationDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Person> Person { get; set; }
+    }
+}
---
+++ APINetCore/APINetCore/Models/Entities/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APINetCore.Models.Entities
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(60)")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(60)")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
+        public string DateOfBirth { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(10)")]
+        public string Age { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(40)")]
+        public string Address { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(40)")]
+        public string City { get; set; }
+
+
+    }
+}
